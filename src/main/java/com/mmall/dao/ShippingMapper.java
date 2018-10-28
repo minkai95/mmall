db83a84c7e87f3cce815ed9f83eebf1ddf6783af
huankai <minkai95@163.com>
2018-10-28 22:19:31 +0800

    完成收货地址管理模块所有功能

--- src/main/java/com/mmall/dao/ShippingMapper.java
+++ src/main/java/com/mmall/dao/ShippingMapper.java
@@ -1,7 +1,10 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Shipping;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ShippingMapper {
@@ -16,4 +19,12 @@ public interface ShippingMapper {
     int updateByPrimaryKeySelective(Shipping record);
 
     int updateByPrimaryKey(Shipping record);
+
+    int deleteByShippingIdUserId(@Param("userId")Integer userId,@Param("shippingId")Integer shippingId);
+
+    int updateByShipping(Shipping record);
+
+    Shipping selectByShippingIdUserId(@Param("userId")Integer userId,@Param("shippingId")Integer shippingId);
+
+    List<Shipping> selectByUserId(@Param("userId")Integer userId);
 }
